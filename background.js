55bacfd689c8a874bcb52c58242fd8a41f1d65d8
FaiChou <faichou.zh@gmail.com>
2021-05-26 14:32:37 +0800

    remove console log

--- background.js
+++ background.js
@@ -3,7 +3,6 @@ function parseImage(info, tab) {
   var imgUrl = info.srcUrl;
   if (imgUrl) {
     var virginUrl = imgUrl.replace(/\.\d+x\d+/, "");
-    console.log(virginUrl);
     chrome.tabs.create({ url: virginUrl });
   }
 }
