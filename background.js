634315994dba18b680bc41ccc992115aa442175f
FaiChou <faichou.zh@gmail.com>
2021-06-23 11:54:15 +0800

    new image on right tab instead the rightest tab

--- background.js
+++ background.js
@@ -1,8 +1,23 @@
 chrome.contextMenus.create({"title": "Open Virgin Image", "contexts": ["image"], "onclick": parseImage});
+
 function parseImage(info, tab) {
   var imgUrl = info.srcUrl;
   if (imgUrl) {
     var virginUrl = imgUrl.replace(/\.\d+x\d+/, "");
-    chrome.tabs.create({ url: virginUrl });
+    var activeTab = getCurrentTabIndex(function(index) {
+      chrome.tabs.create({ url: virginUrl, index: index + 1 });  
+    });
   }
+}
+
+function getCurrentTabIndex(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+  chrome.tabs.query(queryInfo, (tabs) => {
+    var tab = tabs[0];
+    var index = tab.index;
+    callback(index);
+  });
 }
